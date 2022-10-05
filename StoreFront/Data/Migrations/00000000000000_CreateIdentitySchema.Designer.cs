b6c8ae9f07ec0d03ad71a40401d977b54f201262
dypool01 <dypool01@wsc.edu>
2022-10-04 19:09:42 -0500

    Layout is fixed, all errors have also been resolved with administration views etc.

--- StoreFront/Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
+++ StoreFront/Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StoreFront.Data;
+using StoreFront.UI.MVC.Data;
 
 namespace StoreFront.Data.Migrations
 {
