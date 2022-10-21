d2626c5d7c567016f7373fdcfbf5bd4e28eb17b0
dypool01 <dypool01@wsc.edu>
2022-10-21 09:01:25 -0500

    Combed through everytthing, login works, images do not, CRUD does not work as well

--- StoreFront/Program.cs
+++ StoreFront/Program.cs
@@ -6,7 +6,7 @@ using StoreFront.UI.MVC.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")/* ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.")*/;
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 
